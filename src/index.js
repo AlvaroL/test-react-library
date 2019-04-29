13fe1a8cb1f6f101e3ec02e2d57ff32506d3dac7
Alvaro Llorente <a.llorente@lookiero.com>
2019-04-29 10:23:21 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import JsxComponent from 'albareto-test-react';
+// import OtherComponent from 'other-react-test';
+
+// const JsxComponent = require('albareto-test-react');
+const OtherComponent = require('other-react-test').default;
+console.log(OtherComponent);
+
+module.exports = { OtherComponent };
+
+// export default { JsxComponent, OtherComponent };
